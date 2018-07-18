ca0824af47e6a89ad988fe4119a35b917024ff68
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-18 19:10:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,26 +64,17 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
-     */
   
-    /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
-     */
-
-    /**
+    
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
   }
-
-  // We should kick off the animation of the rock around here
-
     top +=2;
     rock.style.top = `${top} px`;// moves rock 
     //We want the rock to move before checking position!!
